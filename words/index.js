53e9822dea613073aaf80d499ddb61a9ea60fec7
komcal <kom.sujautra@gmail.com>
2021-05-03 13:00:30 +0700

    random word api

---
+++ words/index.js
@@ -0,0 +1,11 @@
+import { words as oxfordWords3000 } from './oxford-3000'
+import { words as oxfordWords5000 } from './oxford-3000'
+
+const listWords = [
+  ...oxfordWords3000,
+  ...oxfordWords5000
+]
+
+export {
+  listWords
+}
