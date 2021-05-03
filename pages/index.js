2f5a18dc3a038f31bce8090190a269a8446bd7f9
komcal <kom.sujautra@gmail.com>
2021-05-03 19:52:51 +0700

    change folder name

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Card from '../component/Card';
+import Card from '../components/Card';
 import { useFetchWord } from '../lib/customUseState'
 
 const Index = () => {
